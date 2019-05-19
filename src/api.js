97fe8276086f4a0f38b193161931c43872966969
Bruno Silva <bluisilva@hotmail.com>
2019-05-20 00:58:51 +0100

    removed client id

--- src/api.js
+++ src/api.js
@@ -8,6 +8,6 @@ export default axios.create({
     baseURL: 'https://api.twitch.tv/kraken/',
     headers: {
         Accept: 'application/vnd.twitchtv.v5+json',
-        'Client-ID': 'ethi7sxk5cv2rs479o0t6yje1j1c60'
+        'Client-ID': ''
     }
 });
